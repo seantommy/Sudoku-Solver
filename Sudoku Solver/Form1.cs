21283299419c1720391ede4e26c0a561ca895f2f
Sean Edgar <seantommye@gmail.com>
2019-01-18 15:25:00 -0600

    Basic logic that fills out the puzzle incorrectly without repeating numbers from the same 3x3 box.

--- Sudoku Solver/Form1.cs
+++ Sudoku Solver/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -40,18 +41,123 @@ namespace Sudoku_Solver
 
         private void SolvePuzzle()
         {
+            bool[,] numbersAvailable = InitializeNumbersAvailable();
 
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    if (mandatoryNumbers[x, y] == 0)
+                    {
+                        for (int z = 1; z < 10; z++)
+                        {
+                            if (numbersAvailable[GetCellBlockNumber(x,y), z] == true)
+                            {
+                                dataGrid[x, y].Value = z;
+                                numbersAvailable[GetCellBlockNumber(x, y), z] = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private int GetCellBlockNumber(int x, int y)
+        {
+            int cellBlock;
+
+            if (x < 3)
+            {
+                if (y < 3)
+                {
+                    cellBlock = 1;
+                }
+                else if (y < 6)
+                {
+                    cellBlock = 2;
+                }
+                else
+                {
+                    cellBlock = 3;
+                }
+            }
+            else if (x < 6)
+            {
+                if (y < 3)
+                {
+                    cellBlock = 4;
+                }
+                else if (y < 6)
+                {
+                    cellBlock = 5;
+                }
+                else
+                {
+                    cellBlock = 6;
+                }
+            }
+            else
+            {
+                if (y < 3)
+                {
+                    cellBlock = 7;
+                }
+                else if (y < 6)
+                {
+                    cellBlock = 8;
+                }
+                else
+                {
+                    cellBlock = 9;
+                }
+
+            }
+
+            return cellBlock;
+        }
+
+        private bool[,] InitializeNumbersAvailable()
+        {
+            bool[,] numbersAvailable = new bool[10, 10];
+            for(int x = 1; x < 10; x++)
+            {
+                for(int y = 1; y < 10; y++)
+                {
+                    numbersAvailable[x, y] = true;
+                }
+            }
+
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    int cellBlock = GetCellBlockNumber(x, y);
+
+                    if (mandatoryNumbers[x, y] != 0)
+                    {
+                        numbersAvailable[cellBlock, mandatoryNumbers[x, y]] = false;
+                    }
+                }
+            }
+            return numbersAvailable;
         }
 
         private void SolveButtonClicked(object sender, EventArgs e)
         {
-            for(int x = 0; x < 9; x++)
+            for (int x = 0; x < 9; x++)
             {
-                for(int y = 0; y < 9; y++)
+                for (int y = 0; y < 9; y++)
                 {
-                    if(gridContents[x,y] != 0)
+                    if (gridContents[x, y] != 0)
                     {
                         mandatoryNumbers[x, y] = gridContents[x, y];
+                    }
+                    else
+                    {
+                        mandatoryNumbers[x, y] = 0;
+                        dataGrid[x, y].Style.ForeColor = Color.Blue;
                     }
                 }
             }
@@ -61,9 +167,9 @@ namespace Sudoku_Solver
 
         private void ClearButtonClicked(object sender, EventArgs e)
         {
-            for(int x = 0; x < 9; x++)
+            for (int x = 0; x < 9; x++)
             {
-                for(int y = 0; y < 9; y++)
+                for (int y = 0; y < 9; y++)
                 {
                     dataGrid[x, y].Value = null;
                 }
@@ -72,9 +178,8 @@ namespace Sudoku_Solver
 
         private void CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
-            DataGridViewCell changedCell = dataGrid.CurrentCell;
+            DataGridViewCell changedCell = dataGrid[e.ColumnIndex, e.RowIndex];
             bool numberInput = false;
-
             if (changedCell.Value == null)
             {
                 gridContents[changedCell.ColumnIndex, changedCell.RowIndex] = 0;
@@ -112,7 +217,7 @@ namespace Sudoku_Solver
         {
             DataGridViewCell cell = dataGrid.CurrentCell;
             string key = e.KeyChar.ToString();
-            if(key == "\b")
+            if (key == "\b")
             {
                 cell.Value = null;
             }
