da7a8f3feadfb33cde2e4cf1a890f3ae4ef2d4b0
Sean Edgar <seantommye@gmail.com>
2019-01-18 13:23:59 -0600

    Add project files.

---
+++ Sudoku Solver/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Sudoku_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.solveButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.AllowUserToResizeColumns = false;
+            this.dataGrid.AllowUserToResizeRows = false;
+            this.dataGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGrid.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGrid.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGrid.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGrid.Location = new System.Drawing.Point(42, 12);
+            this.dataGrid.MultiSelect = false;
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersVisible = false;
+            this.dataGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGrid.Size = new System.Drawing.Size(700, 700);
+            this.dataGrid.TabIndex = 0;
+            this.dataGrid.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.CellValueChanged);
+            // 
+            // solveButton
+            // 
+            this.solveButton.Location = new System.Drawing.Point(710, 749);
+            this.solveButton.Name = "solveButton";
+            this.solveButton.Size = new System.Drawing.Size(75, 23);
+            this.solveButton.TabIndex = 1;
+            this.solveButton.Text = "Solve";
+            this.solveButton.UseVisualStyleBackColor = true;
+            this.solveButton.Click += new System.EventHandler(this.SolveButtonClicked);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(598, 749);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButtonClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 812);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.solveButton);
+            this.Controls.Add(this.dataGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button solveButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Sudoku Solver/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku_Solver
+{
+    public partial class Form1 : Form
+    {
+        private int[,] gridContents = new int[9, 9];
+
+        public Form1()
+        {
+            InitializeComponent();
+            BuildGrid();
+        }
+
+        private void BuildGrid()
+        {
+            dataGrid.BackgroundColor = SystemColors.Control;
+            dataGrid.AutoSize = true;
+            for (int x = 0; x < 9; x++)
+            {
+                DataGridViewTextBoxColumn newColumn = new DataGridViewTextBoxColumn();
+                newColumn.Width = 70;
+                DataGridViewRow newRow = new DataGridViewRow();
+                newRow.Height = 70;
+
+                dataGrid.Columns.Add(newColumn);
+                dataGrid.Rows.Add(newRow);
+
+            }
+        }
+
+        private void SolveButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClearButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewCell changedCell = dataGrid.CurrentCell;
+            bool numberInput = false;
+
+            Console.WriteLine(changedCell.Value);
+            for (int x = 1; x < 10; x++)
+            {
+                if (changedCell.Value.ToString() == x.ToString())
+                {
+                    numberInput = true;
+                    Console.WriteLine("numberInput = true");
+                }
+            }
+
+            if (numberInput)
+            {
+                gridContents[changedCell.ColumnIndex, changedCell.RowIndex] = Int16.Parse(changedCell.Value.ToString());
+            }
+            else
+            {
+                if (gridContents[changedCell.ColumnIndex, changedCell.RowIndex] != 0)
+                {
+                    changedCell.Value = gridContents[changedCell.ColumnIndex, changedCell.RowIndex];
+                }
+                else
+                {
+                    changedCell.Value = "";
+                }
+            }
+
+        }
+    }
+}
